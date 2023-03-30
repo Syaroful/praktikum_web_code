172cdbd08734b4b44daa79e45a720ad24c084855
Syaroful <syaroful10403@gmail.com>
2023-03-30 09:44:35 +0700

    Membuat Master View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,31 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index',[
+        'tittle' => 'Home'
+    ]);
 });
+route::get('/about', function () {
+    return view('about',[
+        'tittle' => 'About'
+    ]);
+});
+route::get('/blog', function () {
+    return view('blog',[
+        'tittle' => 'Blog'
+    ]);
+});
+route::get('/contact', function () {
+    return view('contact',[
+        'tittle' => 'Contact Us'
+    ]);
+});
+route::get('/post-details', function () {
+    return view('post-details',[
+        'tittle' => 'Post Detail'
+    ]);
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
